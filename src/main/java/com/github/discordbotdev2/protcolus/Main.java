b7690d6f59bbfcfe68449023b4e76264ce605456
DiscordBotDev2 <84189903+DiscordBotDev2@users.noreply.github.com>
2021-11-02 20:35:39 +0100

    Initial commit

---
+++ src/main/java/com/github/discordbotdev2/protcolus/Main.java
@@ -0,0 +1,36 @@
+package com.github.discordbotdev2.protcolus;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        ProtocolManager manager = ProtocolLibrary.getProtocolManager();
+        manager.addPacketListener(new PacketAdapter(this, ListenerPriority.NORMAL, PacketType.Play.Client.POSITION) {
+            @Override
+            public void onPacketReceiving(PacketEvent event) {
+                PacketContainer packet = event.getPacket();
+                Player player = event.getPlayer();
+                double x = packet.getDoubles().read(0);
+                double y = packet.getDoubles().read(1);
+                double z = packet.getDoubles().read(2);
+                boolean isOnGround = packet.getBooleans().read(0);
+            }
+        });
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/com/github/discordbotdev2/protcolus/commands/boomCommand.java
@@ -0,0 +1,54 @@
+package com.github.discordbotdev2.protcolus.commands;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketContainer;
+import com.github.discordbotdev2.protcolus.Main;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.lang.reflect.InvocationTargetException;
+
+
+public class boomCommand implements CommandExecutor {
+    private Main plugin;
+    public boomCommand(Main plugin) {
+        this.plugin = plugin;
+
+        plugin.getCommand("boom").setExecutor(this);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+
+            ProtocolManager manager = ProtocolLibrary.getProtocolManager();
+
+            player.getLineOfSight(null,50).stream()
+                    .filter(block -> block.getType() != Material.AIR)
+                    .forEach(block -> {
+
+                        Location blockLocation = block.getLocation();
+
+                        PacketContainer packet = manager.createPacket(PacketType.Play.Server.EXPLOSION);
+                        packet.getDoubles().write(0, blockLocation.getX());
+                        packet.getDoubles().write(1,blockLocation.getY());
+                        packet.getDoubles().write(2,blockLocation.getZ());
+
+                        try {
+                            manager.sendServerPacket(player, packet);
+                        } catch (InvocationTargetException e) {
+                            e.printStackTrace();
+                        }
+
+                    });
+        }
+        return true;
+    }
+}
